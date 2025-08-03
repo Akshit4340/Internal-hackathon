7560df80bbd64d0b1466d53622724bd8793523e8
Akshit4340 <24ce01005@iitbbs.ac.in>
2025-08-03 16:25:23 +0530

    start-backend

---
+++ index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import CookieParser from "cookieparser";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(CookieParser());
+
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

a5b628f626ff397c7ed3e3b82a3a94e044336592
Akshit4340 <24ce01005@iitbbs.ac.in>
2025-08-03 16:38:52 +0530

    databse-connected

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import connectDb from "./config/db.js";
 import cors from "cors";
 import CookieParser from "cookieparser";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+connectDb();
 // app.use(CookieParser());
 
 // app.use(
